8afd50a89bb31c65b8c87261b83d27d649ad0e50
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-14 07:11:19 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -2,7 +2,7 @@ class Testbrew < Formula
   desc "Test Brew"
   homepage "https://github.com/testing-1-dot/"
   url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-  sha256 "194f059de8c45fff76bb7f05439765b18d5bc4854419c3bdf40330472ca2b070"
+  sha256 "f1c077fcdae589d58e47c0b8b9afd0eb217d3e20170e4823d2b9f7480c1d002e"
   version "1.3"
   depends_on "python@3.12"
 
