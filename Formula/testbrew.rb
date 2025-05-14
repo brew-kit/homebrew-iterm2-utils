20630ac30b61ded2ddd1143bdb11236440d4c828
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-14 06:57:22 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -2,13 +2,13 @@ class Testbrew < Formula
   desc "Test Brew"
   homepage "https://github.com/testing-1-dot/"
   url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-  sha256 "95b11cae974dfe68d75d0c37f633c3f3096ab0e829b45df1bfaad7256c5da6fc"
+  sha256 "194f059de8c45fff76bb7f05439765b18d5bc4854419c3bdf40330472ca2b070"
   version "1.3"
   depends_on "python@3.12"
 
   def install
     bin.install Dir["*"]
-    system "/opt/homebrew/opt/python@3.12/bin/pip3.12 install -v --no-index --upgrade --isolated pip"
+    system "/opt/homebrew/opt/python@3.12/bin/pip3.12 install -v --no-index --upgrade --isolated --break-system-packages pip"
     system "/opt/homebrew/opt/python@3.12/bin/python3.12 -m venv --system-site-packages --without-pip #{libexec}"
     system "/opt/homebrew/opt/python@3.12/bin/pip3.12 --python=#{libexec}/bin/python install pyobjc"
     system "#{libexec}/bin/python #{bin}/demo.py"
