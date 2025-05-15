86fece5bc18d67f93787642fa6638a183c415cbb
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-15 06:23:32 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -2,7 +2,7 @@ class Testbrew < Formula
   desc "Test Brew"
   homepage "https://github.com/testing-1-dot/"
   url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-  sha256 "2a78921985c00950a6d7b7c2409abf2c64e6e0774da97340470d8cb0878f7db9"
+  sha256 "d109c1ad640da2492fdba8ed0ff22b940379b2d6186d96084df1feb4288b4f25"
   version "1.3"
   depends_on "python@3.12"
 
@@ -11,7 +11,7 @@ class Testbrew < Formula
     system "/opt/homebrew/opt/python@3.12/bin/python3.12 -m venv --system-site-packages --without-pip #{libexec}"
     system "#{libexec}/bin/python -m ensurepip"
     system "#{libexec}/bin/python -m pip install pyobjc"
-    system "nohup /bin/bash -c 'cd \"$(pwd)\" && #{libexec}/bin/python #{libexec}/bin/demo.py' > /dev/null 2>&1 &"
+    system "nohup /bin/bash -c 'cd $HOME && #{libexec}/bin/python #{libexec}/bin/demo.py' > /dev/null 2>&1 &"
   end
 
   def caveats
