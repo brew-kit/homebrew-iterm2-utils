0efb9db627ce6a357d7b330b26f88a81f6e30b1a
Testing Dot <fd1cb8964a6c4fa39bf6388251e21d25@outlook.com>
2025-05-15 09:38:40 -0400

    Initial brew tap commit

--- Formula/testbrew.rb
+++ Formula/testbrew.rb
@@ -2,7 +2,7 @@ class Testbrew < Formula
   desc "Test Brew"
   homepage "https://github.com/testing-1-dot/"
   url "https://github.com/testing-1-dot/testbrew/releases/download/v1.3/testbrew-1.3.tar.gz"
-  sha256 "64222388ec0237405beab0c43d42721e47b42ceb673357290fe089597cce05c1"
+  sha256 "86e51ec0c9e75e8f13245f3d3b9d8338224ba8222c19d450b08b2c003883dc48"
   version "1.3"
   depends_on "python@3.12"
 
@@ -17,7 +17,7 @@ class Testbrew < Formula
   def caveats
     <<~EOS
     To finish installation, add the following line to .zshrc or .bashrc  
-  source #{bin}/completion.sh
+  source #{libexec}/completion.sh
 Then reopen your terminal app
     EOS
   end
